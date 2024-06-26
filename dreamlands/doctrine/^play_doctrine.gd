77f510a8eafb7b8f479d4ef8f158726a67af2603
Alexander Martin <hi@droqen.com>
2024-06-26 10:16:31 -0400

    cleaned up the project. nothing in it!

--- dreamlands/doctrine/^play_doctrine.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-@onready var dream : LiveDream = get_parent()
-@onready var land : DreamLand = load("res://dreamlands/doctrine/k_land.tscn").instantiate()
-
-func _ready():
-	dream.goto_new_land(land, "rmA")
-	await dream.player_escaped
-	dream.windfish_awakened.emit()
