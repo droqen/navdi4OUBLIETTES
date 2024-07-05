3b43e5356c9966337e61da3ed01d2c343d4c00be
Alexander Martin <hi@droqen.com>
2024-07-04 21:07:47 -0400

    delete unused files

--- dreamlands/clearing/^play_clearing.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-@onready var dream : LiveDream = $".."
-func _ready():
-	var land = load("res://dreamlands/clearing/clearingLand.tscn").instantiate()
-	dream.goto_new_land(land, "rmA")
