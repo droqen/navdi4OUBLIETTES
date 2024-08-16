25da2083a0c0af775a8fa9b1da49a63b93bb697f
Alexander Martin <hi@droqen.com>
2024-08-15 22:19:17 -0400

    deleted unused old projects

--- dreamlands/anothertest/^play_another.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-func play(dream:LiveDream) -> void:
-	dream.goto_new_land(
-		load(
-			"res://dreamlands/anothertest/anotherland.tscn"
-		).instantiate(),
-		"rmA"
-	)
--- dreamlands/smalltest/^play_smalltest.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-func play(dream:LiveDream) -> void:
-	dream.goto_new_land(
-		load("res://dreamlands/smalltest/smallland.tscn").instantiate(),
-		"rmA"
-	);
