9546b09cb689f716a0f67618ffd55001ef944e14
Alexander Martin <hi@droqen.com>
2024-08-19 17:50:57 -0400

    code execution test

---
+++ code.gd
@@ -0,0 +1,7 @@
+@tool
+extends EditorScript
+func _run() -> void:
+	print("you pressed CTRL+SHIFT+X in the script editor!")
+	print("so, uh, what did you want?")
+	var MONTHS = ['0','janury']
+	print(MONTHS[2])
