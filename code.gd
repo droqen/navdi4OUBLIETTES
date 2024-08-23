1653340f5e737921c293118a0435b05ded390405
Alexander Martin <hi@droqen.com>
2024-08-23 17:17:55 -0400

    fixed code.gd (no need for the err message)

--- code.gd
+++ code.gd
@@ -4,4 +4,4 @@ func _run() -> void:
 	print("you pressed CTRL+SHIFT+X in the script editor!")
 	print("so, uh, what did you want?")
 	#var MONTHS = ['0','janury']
-	print(MONTHS[2])
+	#print(MONTHS[2])
