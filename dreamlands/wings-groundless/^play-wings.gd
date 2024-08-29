c02f7bf8ace74f1601326e06079bb98e2d75e057
Alexander Martin <hi@droqen.com>
2024-08-29 19:05:25 -0400

    WINGS-GROUNDLESS - beatable basically

--- dreamlands/wings-groundless/^play-wings.gd
+++ dreamlands/wings-groundless/^play-wings.gd
@@ -4,3 +4,15 @@ func play(d:LiveDream)->void:
 		load("res://dreamlands/wings-groundless/groundLAND.tscn").instantiate(),
 		"rmA"
 	)
+	var leftfrom : String = 'NOWHERE'
+	await d.player_escaped.connect(func(escapedfrom):
+		match escapedfrom:
+			'rmEternalFlight':
+				d.windfish_lucidwake.emit(leftfrom)
+			_:
+				leftfrom = escapedfrom
+				d.goto_new_land(
+				load("res://dreamlands/wings-groundless/groundLAND.tscn").instantiate(),
+					"rmEternalFlight"
+				)
+	)
