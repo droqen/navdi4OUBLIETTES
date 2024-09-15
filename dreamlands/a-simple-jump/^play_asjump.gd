af8168b5bf2df7a7669d47664fca16654e02d81b
Alexander Martin <hi@droqen.com>
2024-09-15 12:13:12 -0400

    A SIMPLE JUMP -> 'MEANING OF JUMP A'

--- dreamlands/a-simple-jump/^play_asjump.gd
+++ dreamlands/a-simple-jump/^play_asjump.gd
@@ -6,3 +6,11 @@ func play(d:LiveDream)->void:
 		).instantiate(),
 		"rmA"
 	)
+	await d.player_escaped
+	d.windfish_lucidwake.emit("A")
+	d.goto_new_land(
+		load(
+			"res://dreamlands/a-simple-jump/asjumpland.tscn"
+		).instantiate(),
+		"rmEnd"
+	)
