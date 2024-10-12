b0365047bad7615e4f86daeb137db99151759496
Alexander Martin <hi@droqen.com>
2024-10-11 21:27:34 -0400

    earliest - finished. changed name to "suspended fall"

--- dreamlands/07earliest/^play_earliest.gd
+++ dreamlands/07earliest/^play_earliest.gd
@@ -7,4 +7,12 @@ func play(d:LiveDream):
 		).instantiate(),
 		"rmA"
 	)
+	await d.player_escaped
+	d.windfish_lucidwake.emit("END")
+	d.goto_new_land(
+		load(
+			"res://dreamlands/07earliest/earliestLand.tscn"
+		).instantiate(),
+		"rm6"
+	)
 	
