181b3b33ec8d5f928da5542cc8c1fc31c1491728
Alexander Martin <hi@droqen.com>
2024-10-13 21:58:58 -0400

    CAVERNOUS - consequences

--- dreamlands/13cavernous/player.gd
+++ dreamlands/13cavernous/player.gd
@@ -1,15 +1,31 @@
-extends RigidBody2D
+extends NavdiSolePlayerRigid
 
-enum { JUMPBUF=1, FLORBUF, }
+enum { JUMPBUF=1, FLORBUF, TURNBUF, }
 var bufs = Bufs.Make(self).setup_bufons([
-	JUMPBUF,4,FLORBUF,4,
+	JUMPBUF,4,FLORBUF,4,TURNBUF,8,
 ])
 
+var crouch : bool = false
+
+@onready var lastfrmpos : Vector2 = position
+
 func _physics_process(_delta: float) -> void:
+	if lastfrmpos.distance_to(position) > 5:
+		var p = get_parent()
+		p.remove_child(self)
+		p.add_child(self)
+	lastfrmpos = position
+	
 	var dpad = Pin.get_dpad()
 	if Pin.get_jump_hit(): bufs.on(JUMPBUF)
+	if bufs.try_eat([JUMPBUF,FLORBUF]):
+		linear_velocity.y = -70 # jumpspd
 	if linear_velocity.y>=0 and $floorcast.is_colliding():
 		bufs.on(FLORBUF)
+	if bufs.has(FLORBUF) and not dpad.x and Pin.get_plant_hit():
+		crouch = true
+	if crouch and (not bufs.has(FLORBUF) or dpad.x):
+		crouch = false
 	if linear_velocity.y<0 and not Pin.get_jump_held():
 		gravity_scale = 0.30
 	else:
@@ -17,22 +33,24 @@ func _physics_process(_delta: float) -> void:
 	linear_velocity.x = move_toward(
 		linear_velocity.x, dpad.x * 50, 7)
 	angular_velocity = -rotation
-	if bufs.try_eat([JUMPBUF,FLORBUF]):
-		linear_velocity.y = -70 # jumpspd
 	if dpad.x:
-		$spr.flip_h = dpad.x < 0
+		if $spr.flip_h != (dpad.x < 0):
+			$spr.flip_h = dpad.x < 0
+			bufs.on(TURNBUF)
 	if bufs.has(FLORBUF):
-		if dpad.x: $spr.setup([11,12,13,14],8)
-		else: $spr.setup([10])
+		if crouch: $spr.setup([22])
+		elif bufs.has(TURNBUF): $spr.setup([21])
+		elif dpad.x: $spr.setup([11,12,13,14],8)
+		else: $spr.setup([10,20],60)
 	else:
 		$spr.setup([13])
 	
-	var newpos = Vector2(
-		fposmod(position.x, 180),
-		fposmod(position.y, 200)
-	)
-	if newpos!=position:
-		var p = get_parent()
-		p.remove_child(self)
-		position = newpos
-		p.add_child(self)
+	#var newpos = Vector2(
+		#fposmod(position.x, 180),
+		#fposmod(position.y, 200)
+	#)
+	#if newpos!=position:
+		#var p = get_parent()
+		#p.remove_child(self)
+		#position = newpos
+		#p.add_child(self)
---
+++ navdi4/main/navdisoleplayer_rigid.gd
@@ -0,0 +1,57 @@
+extends RigidBody2D
+class_name NavdiSolePlayerRigid
+
+@warning_ignore("unused_signal")
+signal travelled_dir(travel_dir)
+signal escaped(code)
+
+const SOLE_PLAYER_GROUP_NAME = '__nsp'
+
+@export var player_parent_group : String = "PlayerParent"
+@export var replaces_player_if_different : bool = false
+
+static func ClearPlayer(node_in_tree : Node) -> void:
+	var player = GetPlayer(node_in_tree)
+	if player:
+		player.remove_from_group(SOLE_PLAYER_GROUP_NAME)
+		player.queue_free()
+static func GetPlayer(node_in_tree : Node) -> NavdiSolePlayer:
+	return node_in_tree.get_tree().get_first_node_in_group(SOLE_PLAYER_GROUP_NAME)
+
+func _ready() -> void:
+	var prev_player = GetPlayer(self)
+	if prev_player == null:
+		# i'm the new player, let's go
+		add_to_group(SOLE_PLAYER_GROUP_NAME)
+		for parent in get_tree().get_nodes_in_group(player_parent_group):
+			reparent.call_deferred(parent);
+			break; # only do this for the first parent.
+	else:
+		# a player pre-exists.
+		if (replaces_player_if_different
+		and prev_player.scene_file_path != self.scene_file_path):
+			# i want to replace the player.
+			on_replace_player(prev_player)
+			prev_player.queue_free() # and goodbye to the old.
+			add_to_group(SOLE_PLAYER_GROUP_NAME)
+			for parent in get_tree().get_nodes_in_group(player_parent_group):
+				reparent.call_deferred(parent);
+				break; # only do this for the first parent.
+		else:
+			# if i want to be kept: hide and delete me
+			hide(); queue_free(); return;
+	
+	var dream = LiveDream.GetDream(self)
+	if dream: dream.sole_player_replaced.emit(prev_player, self)
+
+func escape(code):
+	if is_instance_valid(self):
+		escaped.emit(code)
+		deplayer() # always delete a player who escapes.
+
+func deplayer():
+	remove_from_group(SOLE_PLAYER_GROUP_NAME)
+	hide(); queue_free();
+
+func on_replace_player(prev_player):
+	position = prev_player.position
